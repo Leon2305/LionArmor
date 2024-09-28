b696fe6c59338ce92b5d33cba8b3cb7a3e594bfe
LionScripts <96053933+Leon2305@users.noreply.github.com>
2024-09-28 21:21:57 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,9 @@
 fx_version 'cerulean'
 game { 'gta5' }
 
+author 'Leon'
+version '1.0.0'
+
 lua54 'yes'
 shared_scripts {
     'config.lua',
@@ -8,11 +11,9 @@ shared_scripts {
 }
 
 client_scripts {
-	--'@NativeUILua_Reloaded/src/NativeUIReloaded.lua',
 	'client.lua'
 }
 
 server_scripts {
-	--'@oxmysql/lib/MySQL.lua',
     'server.lua',
 }
